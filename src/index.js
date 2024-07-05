7e08e8b23dd5e2d9e11ed10ff83f8d6c99f23863
Intanosaurusss <aeni6746@gmail>
2024-07-05 21:05:47 +0700

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import AllPage from './slide/AllPage';
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AllPage />
-  </React.StrictMode>
-);
